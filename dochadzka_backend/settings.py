7c9ee1fe0e6d6e2536ab8a60c8aeca8954eb2ec6
kurri <tomikbez@gmail.com>
2025-07-22 02:30:56 +0200

    Fix: odstránenie None kategórií z rolí pre stabilitu v TestFlight

--- dochadzka_backend/settings.py
+++ dochadzka_backend/settings.py
@@ -81,14 +81,13 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # ⬅️ musí byť tu
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-
 ]
 
 
