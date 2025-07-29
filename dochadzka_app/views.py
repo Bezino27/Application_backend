5418db933468c57cbab39b0c1af747aea941fcda
kurri <tomikbez@gmail.com>
2025-07-29 23:44:13 +0200

    Pridanie chatu emoji

--- dochadzka_app/views.py
+++ dochadzka_app/views.py
@@ -1,6 +1,5 @@
 # odstránil som import z allauth.conftest
 from django.shortcuts import get_object_or_404
-from numpy import number
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
