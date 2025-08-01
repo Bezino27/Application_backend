dc84d7a8de8c531e7d624eb9524ae89d2fdf2653
kurri <tomikbez@gmail.com>
2025-08-01 11:48:34 +0200

    Uprava settings-new server

--- dochadzka_backend/settings.py
+++ dochadzka_backend/settings.py
@@ -161,10 +161,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME', 'dochadzka'),
-            'USER': os.getenv('DB_USER', 'postgres'),
-            'PASSWORD': os.getenv('DB_PASSWORD', 'MojeSuperTajneHeslo123'),
-            'HOST': os.getenv('DB_HOST', 'db'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
             'PORT': '5432',
         }
     }
