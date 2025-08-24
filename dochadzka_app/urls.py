a2d922bf2100b9ee56db329aaa30c62d88c17a7a
kurri <tomikbez@gmail.com>
2025-08-24 12:24:19 +0200

    edit training

--- dochadzka_app/urls.py
+++ dochadzka_app/urls.py
@@ -6,7 +6,7 @@ from .views import (me_view, login_view, save_expo_push_token, register_user, ch
                     get_categories, player_trainings_view, add_reaction, delete_training_view, training_attendance_view,
                     set_training_attendance, list_clubs, test_push, create_training_view, user_categories_view,
                     training_detail_view, coach_trainings_view, change_password_view, chat_messages_view,
-                    users_in_club, assign_role, remove_role, categories_in_club, coach_players_view, TrainingUpdateView,
+                    users_in_club, assign_role, remove_role, categories_in_club, coach_players_view, training_update_view,
                     all_players_with_roles, player_trainings_history_view, positions_list, player_matches_view,
                     create_match_participation, create_match_view, jersey_numbers_view, club_documents_view,
                     upload_document, match_delete_view, match_stats_view, match_detail_view,match_nominations_view, player_nominated_matches_view, match_participation_view)
@@ -58,6 +58,6 @@ urlpatterns = [
     path("match-participation/", match_participation_view),
     path("match-stats/<int:match_id>/", match_stats_view, name="match-stats"),
     path('matches/delete/<int:match_id>/', match_delete_view, name='match-delete'),
-    path("trainings/<int:taining_id>/", TrainingUpdateView, name="training-update"),
+    path("trainings/<int:taining_id>/", training_update_view, name="training-update"),
 
 ]
