48d2cdcc2f8506b31269d414a0aada9eefb3a1c2
kurri <tomikbez@gmail.com>
2025-08-29 14:20:05 +0200

    match notifications

--- dochadzka_app/urls.py
+++ dochadzka_app/urls.py
@@ -12,7 +12,7 @@ from .views import (me_view, login_view, save_expo_push_token, register_user, ch
                     create_match_participation, create_match_view, jersey_numbers_view, club_documents_view,
                     upload_document, match_delete_view, assign_players_to_category, match_stats_view, match_detail_view,
                     match_nominations_view, player_nominated_matches_view, admin_member_payments,
-                    match_participation_view, club_payment_settings_detail, upload_payments_csv,
+                    match_participation_view, club_payment_settings_detail,
                     upload_pdf_statement_chatgpt,
                     set_preferred_role, club_payment_settings_list, member_payments, create_member_payments,
                     update_member_payment, remind_attendance_view, remind_match_attendance_view)
@@ -76,7 +76,6 @@ urlpatterns = [
     path('create-member-payments/', create_member_payments),
     path('member-payments/<int:pk>/', update_member_payment),
     path('admin-member-payments/', admin_member_payments),
-    path("upload-payments-csv/", upload_payments_csv, name="upload-payments-csv"),
     path("upload-pdf-chatgpt/", upload_pdf_statement_chatgpt),
     path("matches_edit/<int:match_id>/", update_match_view),
     path("remind-attendance/", remind_attendance_view),
