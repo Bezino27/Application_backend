f7f9c48dc552a7369e668b59c01365558a028dd1
kurri <tomikbez@gmail.com>
2025-09-30 15:29:02 +0200

    reset hesla

---
+++ dochadzka_app/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "dochadzka_app.apps.DochadzkaAppConfig"
--- dochadzka_app/apps.py
+++ dochadzka_app/apps.py
@@ -1,10 +1,8 @@
 from django.apps import AppConfig
-from django.apps import AppConfig
 
+class DochadzkaAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dochadzka_app"
 
-class AttendanceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dochadzka_app'
     def ready(self):
         import dochadzka_app.signals  # načíta signály
-
